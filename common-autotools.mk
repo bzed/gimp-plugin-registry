5d8ccc53d3048112beceee1861c01be2e52c82b8
bzed <bzed@9c2cd6cd-4e54-406b-9ece-f07a8ab7d216>
2007-10-15 12:54:40 +0000

    * Adding extra checks to ensure that the debian/{copyright,control} were   updated after modifying the description/version/copyright files in the   plugin directories. * Modifying the update-description target to include version informations   which are provided in the plugin directories now. Thanks to Nelson A. de   Oliveira for the suggestion (Closes: #444591). * Enhancing common-autotools.mk to be able to build the Refocus plugin   which needs some extra love due to a bit buggy build system.   - Refocus (transition from gimp-refocus), also including a bugfix which     was reported against the former package. The plugin needs some extra     build-dependencies which were added.   - Smart-Sharpen-Redux   - Exposure Blend, thanks to Jurij Smakov  for the suggestion     (Closes: #446507)
    git-svn-id: https://ssl.recluse.de/svn/debian/trunk/packages/gimp-plugin-registry@320 9c2cd6cd-4e54-406b-9ece-f07a8ab7d216

--- common-autotools.mk
+++ common-autotools.mk
@@ -5,7 +5,8 @@ SRCDIR = ./src
 PREFIX = /usr
 MANDIR = $(PREFIX)/share/man
 INFODIR = $(PREFIX)/share/info
-CONFFLAGS += --prefix=$(PREFIX) --mandir=$(MANDIR) --infodir=$(INFODIR)
+CONFFLAGS += --prefix=$(PREFIX) --mandir=$(MANDIR) --infodir=$(INFODIR) \
+             --bindir=`/usr/bin/gimptool-2.0 --gimpplugindir`/plug-ins
 
 DEB_HOST_GNU_TYPE   ?= $(shell dpkg-architecture -qDEB_HOST_GNU_TYPE)
 DEB_BUILD_GNU_TYPE  ?= $(shell dpkg-architecture -qDEB_BUILD_GNU_TYPE)
@@ -15,6 +16,7 @@ else
   CONFFLAGS += --build $(DEB_BUILD_GNU_TYPE) --host $(DEB_HOST_GNU_TYPE)
 endif
 
+LDFLAGS += -Wl,-z,defs
 
 $(SRCDIR)/config.status: $(SRCDIR)/configure
 ifneq "$(wildcard /usr/share/misc/config.sub)" ""
@@ -26,22 +28,22 @@ endif
 
 	$(PRE-CONFIGURE-HOOK)
 	cd $(SRCDIR); GIMPTOOL="/usr/bin/gimptool-2.0" \
-	              CFLAGS="$(CFLAGS)" \
-	              LDFLAGS="$(LDFLAGS)" \
-	              CPPFLAGS="$(CPPFLAGS)" ./configure $(CONFFLAGS)
+	              CFLAGS="$(CFLAGS) $(EXTRA_CFLAGS)" \
+	              LDFLAGS="$(LDFLAGS) $(EXTRA_LDFLAGS)" \
+	              CPPFLAGS="$(CPPFLAGS) $(EXTRA_CPPFLAGS)" ./configure $(CONFFLAGS)
 	$(POST-CONFIGURE-HOOK)
 
 build: build-stamp
 
 build-stamp: $(SRCDIR)/config.status
 	$(PRE-BUILD-HOOK)
-	cd $(SRCDIR); make
+	make -C $(SRCDIR) MAKEFLAGS=
 	$(POST-BUILD-HOOK)
 	touch $@
 
 install: build-stamp
 	$(PRE-INSTALL-HOOK)
-	cd $(SRCDIR); make install DESTDIR=$(DESTDIR)
+	make -C $(SRCDIR) install DESTDIR=$(DESTDIR)
 	$(POST-INSTALL-HOOK)
 
 clean:
