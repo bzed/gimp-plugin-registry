20ea225bd27bc2fa13cfd8e0979fcf3ac119f995
Bernd Zeimetz <bernd@bzed.de>
2008-06-01 17:24:53 +0200

    Adding seperate+ plugin.

---
+++ separate+/separate+-0.5.1/libgimp/stdplugins-intl.h
@@ -0,0 +1,6 @@
+/*
+ * Names of this file and directory mean nothing,
+ * because this is a dummy path to maintain difference.
+ */
+
+#include "gettext.h"
---
+++ separate+/separate+-0.5.1/tiff.h
@@ -0,0 +1,6 @@
+#ifndef SEPARATE_TIFF_H
+#define SEPARATE_TIFF_H
+
+void separate_save(GimpDrawable *drawable,struct SeparateContext *sc);
+
+#endif
