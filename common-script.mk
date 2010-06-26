f7f437b9ad4188dcbc381cc4156cea6217007f01
Bernd Zeimetz <bernd@bzed.de>
2010-06-26 22:08:20 +0200

    Enhave common-script.mk to handle more than one script.

--- common-script.mk
+++ common-script.mk
@@ -1,6 +1,11 @@
 build:
 
-install:
-	install -m 644 $(SCRIPT) $(DESTDIR)$(PLUGINSCRIPTDIR)
+install: $(SCRIPT:%=$(DESTDIR)$(PLUGINSCRIPTDIR)/%)
+
+$(DESTDIR)$(PLUGINSCRIPTDIR)/%:
+	if ! test -d $(DESTDIR)$(PLUGINSCRIPTDIR); then \
+		install -d -m 755 $(DESTDIR)$(PLUGINSCRIPTDIR) ;\
+	fi
+	install -m 644 $* $@
 
 .PHONY: build install 
