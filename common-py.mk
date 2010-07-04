91c2a72e8d1af44493004f223020f72e04a0acf2
Bernd Zeimetz <bernd@bzed.de>
2010-07-04 13:31:40 +0200

    Enhance common-py.mk.
    - pycompile scripts to ensure they're syntactically correct.
    - handle script_srcdir and multiple scripts.

--- common-py.mk
+++ common-py.mk
@@ -1,6 +1,22 @@
-build:
+ifndef SCRIPT_SRCDIR
+ SCRIPT_SRCDIR := $(CURDIR)
+endif
 
-install:
-	install -m 755 $(SCRIPT) $(DESTDIR)$(PLUGINBINDIR)
+build: $(SCRIPT:%=$(SCRIPT_SRCDIR)/%c)
+
+$(SCRIPT_SRCDIR)/%c: $(SCRIPT_SRCDIR)/%
+	pycompile $*
+
+
+install: $(SCRIPT:%=$(DESTDIR)$(PLUGINSCRIPTDIR)/%)
+
+$(DESTDIR)$(PLUGINSCRIPTDIR)/%: $(SCRIPT_SRCDIR)/%
+	if ! test -d $(DESTDIR)$(PLUGINSCRIPTDIR); then \
+		install -d -m 755 $(DESTDIR)$(PLUGINSCRIPTDIR) ;\
+	fi
+	install -m 755 $(SCRIPT_SRCDIR)/$* $@
+
+clean:
+	rm -f $(SCRIPT_SRCDIR)/*.pyc
 
 .PHONY: build install 
